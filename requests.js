f73fe9f94833dc33f2c95711ccc3f7d38b9b6782
brimoor <brimoor@umich.edu>
2018-04-25 11:54:12 -0700

    cleaning up naming, docs, and linting

--- requests.js
+++ requests.js
@@ -1,7 +1,6 @@
 /*
- * Abstraction/wrapper over the node request module. The
- * baseRequest object allows setting of default base url and
- * a default header for authorization.
+ * Thin wrapper over the node request module that provides conveinence methods
+ * to perform authenticated API requests.
  *
  * Copyright 2018, Voxel51, LLC
  * voxel51.com
@@ -10,61 +9,14 @@
 'use strict';
 
 let request = require('request');
-const CONFIG = require('./config.js');
-let baseRequest = request.defaults({
-  baseUrl: CONFIG.URI,
-  headers: CONFIG.HEADER,
+let config = require('./config.js');
+
+let base = request.defaults({
+  baseUrl: config.BASE_URL,
+  headers: config.HEADER,
 });
 
-exports.get = function(options) {
-  return new Promise(function(resolve, reject) {
-    baseRequest.get(options, async function(err, rsp, body) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(await validate(rsp, body));
-      }
-    });
-  });
-};
-
-exports.post = function(options) {
-  return new Promise(function(resolve, reject) {
-    baseRequest.post(options, async function(err, rsp, body) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(await validate(rsp, body));
-      }
-    });
-  });
-};
-
-exports.delete = function(options) {
-  return new Promise(function(resolve, reject) {
-    baseRequest.delete(options, async function(err, rsp, body) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(await validate(rsp, body));
-      }
-    });
-  });
-};
-
-exports.put = function(options) {
-  return new Promise(function(resolve, reject) {
-    baseRequest.put(options, async function(err, rsp, body) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(await validate(rsp, body));
-      }
-    });
-  });
-};
-
-let validate = exports.validate = function validate(rsp, body) {
+let validate = function validate(rsp, body) {
   return new Promise(function(resolve, reject) {
     if (rsp.statusCode > 299) {
       let error = new Error(body);
@@ -77,4 +29,53 @@ let validate = exports.validate = function validate(rsp, body) {
   });
 };
 
-exports.req = baseRequest;
+exports.get = function(options) {
+  return new Promise(function(resolve, reject) {
+    base.get(options, async function(err, rsp, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(await validate(rsp, body));
+      }
+    });
+  });
+};
+
+exports.post = function(options) {
+  return new Promise(function(resolve, reject) {
+    base.post(options, async function(err, rsp, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(await validate(rsp, body));
+      }
+    });
+  });
+};
+
+exports.delete = function(options) {
+  return new Promise(function(resolve, reject) {
+    base.delete(options, async function(err, rsp, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(await validate(rsp, body));
+      }
+    });
+  });
+};
+
+exports.put = function(options) {
+  return new Promise(function(resolve, reject) {
+    base.put(options, async function(err, rsp, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(await validate(rsp, body));
+      }
+    });
+  });
+};
+
+exports.base = base;
+exports.validate = validate;
