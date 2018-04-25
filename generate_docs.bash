50db90c3ed5d07b29d12667cc41d37089768ab33
hoddr <hoddr@outlook.com>
2018-04-25 15:26:08 -0400

    Updated bash script

--- generate_docs.bash
+++ generate_docs.bash
@@ -1,33 +1,14 @@
 #!/usr/bin/env bash
 # Generate Node Client Library documentation
 #
-# Assumes apidocjs is globally installed:
-#   npm install -g apidocjs
-#
 # Copyright 2018, Voxel51, LLC
 # David Hodgson, david@voxel51.com
 # Brian Moore, brian@voxel51.com
 #
 
 
-# Show usage information
-usage() {
-    echo "Usage:  bash $0 [-h] [-p]
--h      Display this help message.
-"
-}
-
-
-# Parse flags
-SHOW_HELP=false
-while getopts "hp" FLAG; do
-    case $FLAG in
-        h) SHOW_HELP=true ;;
-        *) SHOW_HELP=true ;;
-    esac
-done
-[ $SHOW_HELP = true ] && usage && exit 0
-
+# Usage:
+#   bash generate_docs.bash
 
 echo "**** Generating API documentation"
 ./node_modules/.bin/jsdoc -c conf.json
