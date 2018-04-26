9aaecb05322efacdb083f7ad314bfd3b76f9fb23
brimoor <brimoor@umich.edu>
2018-04-26 15:31:23 -0700

    fixing docstring

--- generate_docs.bash
+++ generate_docs.bash
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-# Generate Node Client Library documentation
+# Generate javascript client library documentation
+#
+# Usage:
+#   bash generate_docs.bash
 #
 # Copyright 2018, Voxel51, LLC
 # David Hodgson, david@voxel51.com
 # Brian Moore, brian@voxel51.com
 #
 
-
-# Usage:
-#   bash generate_docs.bash
-
 echo "**** Generating API documentation"
+
 ./node_modules/.bin/jsdoc -c conf.json
+
 echo "**** API documentation complete"
