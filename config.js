030dfba437db501f7df5d57f0235b88b7f68821f
brimoor <brimoor@umich.edu>
2018-05-06 15:44:07 -0700

    making config a truly static container for config settings

--- config.js
+++ config.js
@@ -1,27 +1,22 @@
 /*
- * Configuration module that provides the base URL, token environment variable,
- * and authorization header for the API.
+ * Configuration module for the API.
  *
  * Copyright 2017-2018, Voxel51, LLC
  * voxel51.com
  *
  * David Hodgson, david@voxel51.com
+ * Brian Moore, brian@voxel51.com
 */
 
 'use strict';
 
-let auth = require('./auth.js');
+let os = require('os');
+let path = require('path');
 
-let config = {
+exports.config = {
   TOKEN_ENVIRON_VAR: "VOXEL51_API_TOKEN",
-  TOKEN_PATH: "~/.voxel51/api-token.json",
+  TOKEN_PATH: path.join(os.homedir(), '.voxel51/api-token.json'),
   ACCESS_TOKEN_FIELD: "access_token",
   PRIVATE_KEY_FIELD: "private_key",
   BASE_URL: 'https://api.voxel51.com/v1/',
-
-  TOKEN: auth.setToken() || process.env.VOXEL51_API_TOKEN,
 };
-
-config.HEADER = {'Authorization': 'Bearer ' + config.TOKEN};
-
-module.exports = config;
