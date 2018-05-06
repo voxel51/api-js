24e70592f9a1a347fb4ddb54e0cb733649fa1d47
brimoor <brimoor@umich.edu>
2018-05-06 15:47:16 -0700

    adding an index.js file so that the submodules can be appropriately referenced

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+ * Main module for the Voxel51 Vision Services API.
+ *
+ * Copyright 2017-2018, Voxel51, LLC
+ * voxel51.com
+ *
+ * Brian Moore, brian@voxel51.com
+*/
+
+'use strict';
+
+exports.api = require('./api.js');
+exports.auth = require('./auth.js');
